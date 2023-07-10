479c1674e66210b6cf5cb69296f5ce02ef9e7000
listenzcc <cc.public@outlook.com>
2023-07-10 11:03:29 +0800

    Init commit

---
+++ util/constant.py
@@ -0,0 +1,58 @@
+"""
+File: constant.py
+Author: Chuncheng Zhang
+Date: 2023-07-10
+Copyright & Email: chuncheng.zhang@ia.ac.cn
+
+Purpose:
+    Amazing things
+
+Functions:
+    1. Requirements and constants
+    2. Function and class
+    3. Play ground
+    4. Pending
+    5. Pending
+"""
+
+
+# %% ---- 2023-07-10 ------------------------
+# Requirements and constants
+import os
+import sys
+import logging
+import numpy as np
+import pandas as pd
+
+from PIL import Image
+from pathlib import Path
+from omegaconf import OmegaConf
+from rich import print, inspect
+from dataclasses import dataclass
+
+from logger import LOGGER
+
+
+# %% ---- 2023-07-10 ------------------------
+# Function and class
+@dataclass
+class ProjectConf:
+    name: str = 'OpenCV-Display'
+    version: str = '0.0.1'
+
+
+conf = OmegaConf.structured(dict(
+    project=ProjectConf
+))
+
+
+# %% ---- 2023-07-10 ------------------------
+# Play ground
+LOGGER.info('Constant is loaded')
+
+# %% ---- 2023-07-10 ------------------------
+# Pending
+
+
+# %% ---- 2023-07-10 ------------------------
+# Pending
---
+++ util/logger.py
@@ -0,0 +1,65 @@
+"""
+File: logger.py
+Author: Chuncheng Zhang
+Date: 2023-07-10
+Copyright & Email: chuncheng.zhang@ia.ac.cn
+
+Purpose:
+    Amazing things
+
+Functions:
+    1. Requirements and constants
+    2. Function and class
+    3. Play ground
+    4. Pending
+    5. Pending
+"""
+
+
+# %% ---- 2023-07-10 ------------------------
+# Requirements and constants
+import logging
+
+
+# %% ---- 2023-07-10 ------------------------
+# Function and class
+class MyLogger(object):
+    name = 'OpenCV-Display'
+    file_handler_log_level = logging.DEBUG
+    file_handler_log_fmt = '%(asctime)s %(name)s %(levelname)-8s %(message)-40s {{%(filename)s:%(lineno)s:%(module)s:%(funcName)s}}'
+    filepath = 'log.txt'
+    stream_handler_log_level = logging.DEBUG
+    stream_handler_log_fmt = '%(asctime)s %(name)s %(levelname)-8s %(message)-40s {{%(filename)s:%(lineno)s}}'
+
+    def __init__(self):
+        self.logger = self.mk_logger()
+
+    def mk_logger(self):
+        logger = logging.getLogger(self.name)
+        logger.setLevel(logging.DEBUG)
+
+        fh = logging.FileHandler(self.filepath)
+        fh.setFormatter(logging.Formatter(self.file_handler_log_fmt))
+        fh.setLevel(self.file_handler_log_level)
+
+        sh = logging.StreamHandler()
+        sh.setFormatter(logging.Formatter(self.stream_handler_log_fmt))
+        sh.setLevel(self.stream_handler_log_level)
+
+        for hdl in [fh, sh]:
+            logger.addHandler(hdl)
+        return logger
+
+
+LOGGER = MyLogger().logger
+
+# %% ---- 2023-07-10 ------------------------
+# Play ground
+
+
+# %% ---- 2023-07-10 ------------------------
+# Pending
+
+
+# %% ---- 2023-07-10 ------------------------
+# Pending
