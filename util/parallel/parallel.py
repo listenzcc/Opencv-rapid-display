1e1112dc89ef71a121a2c947303ac605bde2c10a
listenzcc <cc.public@outlook.com>
2023-07-26 16:33:50 +0800

    Changed parallel method

--- util/parallel/parallel.py
+++ util/parallel/parallel.py
@@ -12,6 +12,7 @@ from . import setPortAddress, setData
 class Parallel(object):
     def __init__(self):
         self.address = None
+        self.buffer = []
         pass
 
     def reset(self, address):
@@ -19,30 +20,40 @@ class Parallel(object):
         address_hex = int(address, 16)
         setPortAddress(address_hex)
         setData(0)
-        self.latest = 0
+        self.run_forever()
+
+    def sending_loop(self):
+        while True:
+            self._send(verbose=True)
+            time.sleep(0.001)
+
+    def run_forever(self):
+        t = threading.Thread(target=self.sending_loop, daemon=True)
+        t.start()
 
     def send(self, value, verbose=True):
-        if self.address is None:
-            print('Send failed since the Parallel is not set')
-            return
+        self.buffer.append(value)
 
-        t = threading.Thread(target=self._send, args=(value, verbose))
-        t.setDaemon(True)
-        t.start()
+        # t = threading.Thread(target=self._send, args=(verbose,), daemon=True)
+        # t.start()
 
         return time.time()
 
-    def _send(self, value, verbose):
-        try:
-            value = int(value)
-        except Exception as err:
-            print('Exception: %s' % err)
-            value = self.latest + 1
+    def _send(self, verbose):
+        n = len(self.buffer)
+        buffer = set([self.buffer.pop(0) for _ in range(n)])
 
-        setData(value)
-        time.sleep(0.001)
-        setData(0)
+        value = sum(buffer)
 
-        self.latest = value
+        if value == 0:
+            return
+
+        if self.address is None:
+            print('Send failed since the Parallel is not set')
+        else:
+            setData(value)
+            time.sleep(0.001)
+            setData(0)
+
         if verbose:
             print('Sent: {} to {}'.format(value, self.address))
